f28cf02211de87aa4e86d2b8185f714f15b46cd0
yinchengnuo <yinnuo96@163.com>
2020-05-16 20:41:30 +0800

    2020.05.06

--- common/js/uni_request.js
+++ common/js/uni_request.js
@@ -1,5 +1,3 @@
-const STATUS={PENDING:0,FULFILLED:1,REJECTED:2};class MyPromise{constructor(a){this.status=STATUS.PENDING,this.resolveData=null,this.rejectData=null,this.onFulfilledList=[],this.onRejectedList=[],this.onResolve=a=>{this.status===STATUS.PENDING&&(this.status=STATUS.FULFILLED,this.resolveData=a,this.onFulfilledList.forEach(a=>a(this.resolveData)))},this.onReject=a=>{this.status===STATUS.PENDING&&(this.status=STATUS.REJECTED,this.rejectData=a,this.onRejectedList.forEach(a=>a(this.rejectData)))},this.resolvePromise=(a,b,c)=>{if(a instanceof Promise)a.status===STATUS.PENDING?a.then(a=>{this.resolvePromise(a,b,c)},c):a.status===STATUS.FULFILLED?b(a.resolveData):c(a.rejectData);else if(null!==a&&a instanceof Object)try{let d=a.then;d instanceof Function?d.call(a,a=>{this.resolvePromise(a,b,c)},c):b(a)}catch(a){c(a)}else b(a)};try{a(this.onResolve.bind(this),this.onReject.bind(this))}catch(a){this.onReject(a)}}then(a,b){let c;return c=this.status===STATUS.PENDING?new Promise((c,d)=>{this.onFulfilledList.push(()=>{if(!(a instanceof Function))c(this.resolveData);else{let b=a(this.resolveData);this.resolvePromise(b,c,d)}}),this.onRejectedList.push(()=>{if(!(b instanceof Function))d(this.rejectData);else{let a=b(this.rejectData);this.resolvePromise(a,c,d)}})}):this.status===STATUS.FULFILLED?new Promise((b,c)=>{if(!(a instanceof Function))b(this.resolveData);else{let d=a(this.resolveData);this.resolvePromise(d,b,c)}}):new Promise((a,c)=>{if(!(b instanceof Function))c(this.rejectData);else{let d=b(this.rejectData);this.resolvePromise(d,a,c)}}),c}catch(a){a instanceof Function&&(this.status===STATUS.PENDING?this.onRejectedList.push(()=>{null!==this.rejectData&&a(this.rejectData)}):(this.status=STATUS.REJECTED)&&null!==this.rejectData&&a(this.rejectData))}}
-
 export default function ({ baseURL, timeout, header }) {
 	return {
 		get(url, data) { return this.request('GET', url, data) },
@@ -18,7 +16,7 @@ export default function ({ baseURL, timeout, header }) {
 				aborted = true // 将请求状态标记为已取消
 				requestTask ? requestTask.abort() : '' // 执行取消请求方法
 			}, progressUpdateHandle, onProgressUpdate = e => progressUpdateHandle = e // progressUpdateHandle 监听上传进度变化回调，onProgressUpdate 监听上传进度变化方法
-			return new Proxy(new MyPromise((resolve, reject) => { // 返回经过 Proxy 后的 Promise 对象使其可以监听到是否调用 abort 和 onProgressUpdate 方法
+			return new Proxy(new Promise((resolve, reject) => { // 返回经过 Proxy 后的 Promise 对象使其可以监听到是否调用 abort 和 onProgressUpdate 方法
 				this.interceptors.request.intercept({ header: data.header || {}, body: data.formData || {} }, method, url, data).then(async ({ header, body }) => { // 等待请求拦截器里的方法执行完
 					if (aborted) { // 如果请求已被取消,停止执行,返回 reject
 						await this.onerror(method, url, data, '网络请求失败：主动取消')
@@ -33,7 +31,7 @@ export default function ({ baseURL, timeout, header }) {
 					    success: async res => {
 							clearTimeout(timer)
 							res.statusCode !== 200 ? await this.onerror(method, url, data, `网络请求异常：服务器响应异常：状态码：${res.statusCode}`) : '',
-							this.interceptors.response.intercept(res.statusCode === 200 ? resolve : reject, res, method, url, data) // 执行响应拦截器
+							this.interceptors.response.intercept(res.statusCode === 200 ? resolve : reject,  { success: res.statusCode === 200, ...res }, method, url, data) // 执行响应拦截器
 					    },
 						fail: async res => {
 							clearTimeout(timer)
@@ -48,14 +46,14 @@ export default function ({ baseURL, timeout, header }) {
 						reject('网络请求时间超时') // reject 原因
 					}, timeout  || 12345) // 设定检测超时定时器
 				})
-			}), { get: (target, prop) => prop === 'abort' ? abort : prop === 'onProgressUpdate' ? onProgressUpdate : Reflect.get(target, prop) }) // 如果调用 cancel 方法,返回 _watcher.cancel 方法
+			}), { get: (target, prop) => prop === 'abort' ? abort : prop === 'onProgressUpdate' ? onProgressUpdate : Reflect.get(target, prop).bind(target) }) // 如果调用 cancel 方法,返回 _watcher.cancel 方法
 		},
 		request(method, url, data) {
 			let timer, requestTask, aborted = false, abort = () => { // timer 检测超时定时器，requestTask 网络请求 task 对象，aborted 请求是否已被取消，abort 取消请求方法
 				aborted = true // 将请求状态标记为已取消
 				requestTask ? requestTask.abort() : '' // 执行取消请求方法
 			}
-			return new Proxy(new MyPromise((resolve, reject) => { // 返回经过 Proxy 后的 Promise 对象使其可以监听到是否调用 abort 方法
+			return new Proxy(new Promise((resolve, reject) => { // 返回经过 Proxy 后的 Promise 对象使其可以监听到是否调用 abort 方法
 				this.interceptors.request.intercept({ header: header || {}, body: data || {} }, method, url, data).then(async ({ header, body: data }) => { // 等待请求拦截器里的方法执行完
 					if (aborted) { // 如果请求已被取消,停止执行,返回 reject
 						await this.onerror(method, url, data, '网络请求失败：主动取消')
@@ -67,7 +65,7 @@ export default function ({ baseURL, timeout, header }) {
 					    success: async res => { // 网络请求成功
 							clearTimeout(timer) // 清除检测超时定时器
 							res.statusCode !== 200 ? await this.onerror(method, url, data, `网络请求异常：服务器响应异常：状态码：${res.statusCode}`) : '' 
-							this.interceptors.response.intercept(res.statusCode === 200 ? resolve : reject, res, method, url, data) // 执行响应拦截器
+							this.interceptors.response.intercept(res.statusCode === 200 ? resolve : reject, { success: res.statusCode === 200, ...res }, method, url, data) // 执行响应拦截器
 					    },
 						fail: async res => { // 网络请求失败
 							clearTimeout(timer) // 清除检测超时定时器
@@ -81,7 +79,7 @@ export default function ({ baseURL, timeout, header }) {
 						reject('网络请求时间超时') // reject 原因
 					}, timeout  || 12345) // 设定检测超时定时器
 				})
-			}), { get: (target, prop) => prop === 'abort' ? abort : Reflect.get(target, prop) }) // 如果调用 abort 方法,返回 abort 方法
+			}), { get: (target, prop) => prop === 'abort' ? abort : Reflect.get(target, prop).bind(target) }) // 如果调用 abort 方法,返回 abort 方法
 		},
 		interceptors: { // 拦截器
 			request: {
@@ -101,7 +99,13 @@ export default function ({ baseURL, timeout, header }) {
 					for (let i = 0; i < this.interceptors.length; i ++) {
 						response = await this.interceptors[i](response, method, url, data)
 					}
-					return STATUS(method === 'downloadFile' ? response : response.data)
+					if (response.success) {
+						return STATUS(typeof response.data === 'string' ? JSON.parse(response.data) : response.data)
+					} else {
+						delete response.success
+						return STATUS(response, method, url, data)
+					}
+					
 				}
 			}
 		}
