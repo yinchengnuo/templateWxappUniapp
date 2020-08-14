1a41b7a63415e681e6061c105cd551a8646f88c7
尹成诺 <yinnuo96@163.com>
2020-08-14 16:27:24 +0800

    优化代码

--- pages/A/index-component/index-component.vue
+++ pages/A/index-component/index-component.vue
@@ -10,7 +10,8 @@
 		<rich-text :nodes="nodesAfter"></rich-text>
 		<view class="p">这种做法本身没有什么问题，但是过于繁琐。不仅需要在每个页面的模板中都要声明组件，而且还需要用：this.$refs['custom-interactive']... </view>
 		<view class="p">这里实现了一个无需引入组件即可调用组件内部方法的例子，仅供参考。
-			原理是在 vue.config.js 中，通过配置 vue-loader，重写 vue-template-compiler 的 compile 方法。手动在 compile 之前将 custom-interactive 组件插入 template。</view>
+			原理是在 vue.config.js 中，通过配置 vue-loader，重写 vue-template-compiler 的 compile 方法。手动在 compile 之前将 custom-interactive 组件插入 template：</view>
+		<rich-text :nodes="nodesAfterAfter"></rich-text>
 		<button type="primary" @click="_$loading()">试一试 this._$loading()</button>
 	</view>
 </template>
@@ -20,9 +21,33 @@
 		data() {
 			return {
 				nodesBefore: `
-<pre style="border: 1px solid #AAAAAA; border-radius: 8px; background: #FFFAE8; padding: 8px; margin: 8px 0px; overflow: scroll;">&lt;custom-interactive ref="custom-interactive" /&gt;`,
+<pre style="border: 1px solid #AAAAAA; border-radius: 8px; background: #FFFAE8; padding: 8px; margin: 8px 0px; overflow: scroll;">&lt;custom-interactive ref="custom-interactive" /&gt;
+</pre>`,
 				nodesAfter: `
-<pre style="border: 1px solid #AAAAAA; border-radius: 8px; background: #FFFAE8; padding: 8px; margin: 8px 0px; overflow: scroll;">this.$refs['custom-interactive']._$loading();`
+<pre style="border: 1px solid #AAAAAA; border-radius: 8px; background: #FFFAE8; padding: 8px; margin: 8px 0px; overflow: scroll;">this.$refs['custom-interactive']._$loading();
+</pre>`,
+				nodesAfterAfter: `
+<pre style="border: 1px solid #AAAAAA; border-radius: 8px; background: #FFFAE8; padding: 8px; margin: 8px 0px; overflow: scroll;">// vue.config.js
+module.exports = {
+  // ...
+  chainWebpack: config => {
+    config.module.rule('vue')
+	.use('vue-loader')
+	.loader('vue-loader').tap(options => {
+	  const compile = options.compiler.compile
+		options.compiler.compile = (template, ...args) => {
+		  if (args[0].resourcePath.match(/^pages/)) {
+			template = template.replace(/[\\s\\S]+?&lt[\\d\\D]+?&gt/, _ => \`\${_}
+			  &lt;custom-interactive ref="custom-interactive" style="position: fixed; z-index: 123;" /&gt;
+			\`)
+		  }
+		  return compile(template, ...args)
+	    }
+	  return options
+    })
+  }
+}
+</pre>`,
 			};
 		}
 	}
