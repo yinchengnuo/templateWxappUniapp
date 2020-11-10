2a8fbcecceadbb352b1b3f970bf12ebfeafaa84f
yinchengnuo <yinnuo96@163.com>
2020-11-10 16:30:01 +0800

    微调

--- App.vue
+++ App.vue
@@ -20,7 +20,6 @@
 	@import '@/styles/main.css'; //引入 ColorUI 组件库主样式
 	@import '@/styles/icon.css'; //引入 ColorUI 组件库图标样式
 	@import '@/styles/animation.css'; //引入 ColorUI 组件库动画样式
-
 	page {
 		height: 100%;
 		background: #FFFFFF;
