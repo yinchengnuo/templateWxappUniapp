4c682817165ee34dd8a99003bbd89433c079ca31
尹成诺 <yinchengnuo@qq.com>
2023-10-30 09:15:48 +0800

    .

--- pages/聊天/聊天.vue
+++ pages/聊天/聊天.vue
@@ -1,44 +1,53 @@
 <template>
-	<view>
-		<view style="padding-bottom: 84rpx;">
+	<view class="index">
+		<image class=".page_bg" mode="aspectFill" src='/static/page_bg.png'></image>
+		<view class="page_title flex" :style="{
+				marginTop: `${$app().globalData.menuButtonBoundingClientRect.top}px`, height: `${$app().globalData.menuButtonBoundingClientRect.height}px` 
+			}">有问题，问AI</view>
+		<scroll-view scroll-y :scroll-top="scroll"
+			:style="{  height: `calc(100vh - ${$app().globalData.menuButtonBoundingClientRect.bottom}px - 84rpx)` }">
 			<view class="cu-chat">
 				<template v-for="(item, index) in list">
-					<view v-if="item.type === 'Q'" :key="index" class="cu-item self">
-						<view class="main">
+					<view v-if="item.type === 'chat'" :key="index" class="cu-item self"
+						style="padding: 12rpx 24rpx 48rpx;">
+						<view class="main" style="margin: 0 24rpx 0 0; max-width: calc(100% - 210rpx);">
 							<view class="content bg-green shadow">
-								<text>{{ item.message }}</text>
+								<text>{{ item.content }}</text>
 							</view>
 						</view>
 						<open-data class="cu-avatar radius" type="userAvatarUrl"></open-data>
-						<view class="date">{{ item.time }}</view>
+						<view class="date" style="bottom: 10rpx;">
+							<text>{{ item.time }}</text>
+							<text class="cuIcon-lightauto text-green"
+								style="position: absolute; top: -6rpx; left: calc(100% + 48rpx); white-space: nowrap;">-{{ item.energy }}</text>
+						</view>
 					</view>
 
-					<view v-if="item.type === 'A'" :key="index" class="cu-item">
+					<view v-if="item.type === 'reply'" :key="index" class="cu-item" style="padding: 12rpx 24rpx 48rpx;">
 						<view class="cu-avatar radius">
 							<image src="../../static/openai.png" class="w100 h100"></image>
 						</view>
-						<view class="main">
+						<view class="main" style="margin: 0 0 0 24rpx; max-width: calc(100% - 210rpx);">
 							<view class="content shadow">
-								<text>{{ item.message }} <text v-if="ing">...</text> </text>
+								<text>{{ item.content }} <text v-if="ing">...</text> </text>
 							</view>
 						</view>
-						<view class="date ">{{ item.time }}</view>
+						<view class="date" style="bottom: 10rpx;">
+							<text class="cuIcon-lightauto text-green"
+								style="position: absolute; top: -4rpx; right: calc(100% + 48rpx); white-space: nowrap;">-{{ item.energy }}</text>
+							<text>{{ item.time }}</text>
+						</view>
 					</view>
 				</template>
+				<view style="height: 48rpx;"></view>
 			</view>
-
-			<view class="cu-bar foot input" :class="InputBottom!=0?'cur':''" :style="{ bottom: InputBottom + 'px' }">
-				<view class="action">
-					<text class="cuIcon-settings text-grey"></text>
-				</view>
-				<input class="solid-bottom" v-model="text" @focus="InputFocus" @blur="InputBlur" :adjust-position="true"
-					:focus="false" maxlength="300" cursor-spacing="10"
-					@keyboardheightchange="keyboardheightchange"></input>
-				<!-- <view class="action">
-					<text class="cuIcon-emojifill text-grey"></text>
-				</view> -->
-				<button class="cu-btn bg-green shadow" @click="send">发送</button>
+		</scroll-view>
+		<view class="cu-bar foot input">
+			<view class="action">
+				<text class="cuIcon-settings text-grey"></text>
 			</view>
+			<input class="solid-bottom" v-model="chat" @confirm="send"></input>
+			<button class="cu-btn bg-green shadow" @click="send">发送</button>
 		</view>
 	</view>
 </template>
@@ -47,69 +56,69 @@
 	export default {
 		data() {
 			return {
+				list: [],
+				chat: '',
 				ing: false,
-				text: '你好',
-				InputBottom: 0,
-				list: []
+				scroll: 999999999,
 			}
 		},
+		created() {
+			this.$('/chat_record').then(data => {
+				console.log(data)
+				if (data.length) {
+					data.forEach(e => {
+						this.list.push({
+							type: 'chat',
+							content: e.chat,
+							time: e.chat_time,
+							energy: e.promptTokens
+						})
+						this.list.push({
+							type: 'reply',
+							content: e.reply,
+							time: e.reply_time,
+							energy: e.completionTokens
+						})
+						setTimeout(() => this.scroll++)
+					})
+				}
+			})
+		},
 		methods: {
-			InputFocus(e) {
-				// this.InputBottom = e.detail.height
-			},
-
-			InputBlur(e) {
-				// this.InputBottom = 0
-			},
-			keyboardheightchange(e) {
-				console.log(e)
-			},
 			async send() {
 				if (this.ing) {
-					return this.$toast('AI正在生成对话，请稍后')
+					return this.$toast('AI正在生成对话，请稍后再试')
 				}
-				if (this.text.trim()) {
+				if (this.chat.trim()) {
 					this.ing = true
-					this.list.push({
-						type: 'Q',
-						message: this.text,
+					const chat = {
+						energy: 0,
+						type: 'chat',
+						content: this.chat,
 						time: (new Date).toLocaleString()
-					})
-					this.list.push({
+					}
+					const reply = {
 						time: '',
-						type: 'A',
-						message: '',
-					})
-					setTimeout(() => {
-						uni.pageScrollTo({
-							scrollTop: 999999999999
-						})
-					})
-					const channel = new uniCloud.SSEChannel()
-					channel.on('message', (message) => {
-						const item = this.list[this.list.length - 1]
-						item.message += message
-						setTimeout(() => {
-							uni.pageScrollTo({
-								scrollTop: 999999999999
-							})
-						})
-					})
-					channel.on('end', (message) => {
-						console.log('on end', message);
+						energy: 0,
+						content: '',
+						type: 'reply',
+					}
+					this.list.push(chat)
+					this.list.push(reply)
+					setTimeout(() => this.scroll++)
+					this.$('/chat', {
+						chat: this.chat
+					}).then(data => {
+						chat.time = data.chat_time
+						reply.content = data.reply
+						reply.time = data.reply_time
+						chat.energy = data.promptTokens
+						reply.energy = data.completionTokens
+						setTimeout(() => this.scroll++)
+					}).finally(() => {
 						this.ing = false
 					})
-					await channel.open()
-					uniCloud.callFunction({
-						name: 'AI',
-						data: {
-							text: this.text,
-							channel: channel
-						}
-					}).catch(() => {
-						this.ing = false
-					})
-					this.text = ''
+					this.chat = ''
 				} else {
 					this.$toast('请输入文字')
 				}
@@ -118,6 +127,20 @@
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
+	.index {
+		.page_bg {
+			top: 0;
+			left: 0;
+			z-index: 0;
+			width: 100%;
+			height: 100vh;
+			position: fixed;
+		}
 
+		.page_title {
+			font-size: 13pt;
+			font-weight: bold;
+		}
+	}
 </style>
