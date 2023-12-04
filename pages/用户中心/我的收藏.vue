250fdf9913946befd8ca28ae10a65990857afb39
尹成诺 <yinchengnuo@qq.com>
2023-12-04 18:03:55 +0800

    .

--- pages/用户中心/我的收藏.vue
+++ pages/用户中心/我的收藏.vue
@@ -1,26 +1,63 @@
 <template>
-	<Page ref="Page">
-		<view class="index">
-			我的收藏
-		</view>
+	<Page ref="Page" bg type="no">
+		<template v-slot:default="{ page }">
+			<template v-if="page">
+				<template v-if="records.length">
+					<view class="cu-list menu-avatar">
+						<navigator :url="item.page" class="cu-item arrow bg-white solids-bottom" v-for="(item, index) in records"
+							:key="index">
+							<view class="cu-avatar round lg">
+								<image class="w100 h100" :src="item.icon" mode="scaleToFill" />
+							</view>
+							<view class="content">
+								<view class="text-bold">{{ item.name }}</view>
+								<view class="text-gray text-sm flex justify-start">
+									<text class="text-cut">
+										{{ item.desc }}
+									</text>
+								</view>
+							</view>
+							<view class="action">
+								<view class="cuIcon-right"></view>
+							</view>
+						</navigator>
+					</view>
+				</template>
+			</template>
+		</template>
 	</Page>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-
-			}
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			records: [],
+			empty: false,
+		};
+	},
+	computed: {
+		loading() {
+			return this.$store.state.app.loading
 		}
-	}
+	},
+	created() {
+		this.$loading()
+		this.$("/collect_record").then(data => {
+			this.records = this.$store.state.app.list.filter(e => data.find(ee => ee.function_id === e._id)).map(e => ({ ...e, name: e.page.split('/').at(-1) }));
+			if (!this.records.length) {
+				this.empty = true
+			}
+		}).finally(() => {
+			this.$loaded()
+		});
+	},
+	mounted() {
+		this.$refs.Page.getHeight();
+		this.$refs.ADFloat.show = false;
+		clearInterval(this.$refs.Page.timer);
+	},
+};
 </script>
 
-<style lang="scss" scoped>
-	.index {
-		height: 100%;
-	}
-</style>
+<style lang="scss" scoped></style>
