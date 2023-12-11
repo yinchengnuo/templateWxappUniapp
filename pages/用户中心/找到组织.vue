bcf075906f395a18a1789da717afa0103519bc4a
尹成诺 <yinchengnuo@qq.com>
2023-12-11 14:38:00 +0800

    .

--- pages/用户中心/找到组织.vue
+++ pages/用户中心/找到组织.vue
@@ -2,7 +2,7 @@
   <Page ref="Page">
     <template v-slot:default="{ page }">
       <template v-if="page">
-        <image class="w100" :src="src" @click="$preview(src)" show-menu-by-longpress mode="widthFix" />
+        <image class="w100" :src="user.group" @click="$preview(user.group)" show-menu-by-longpress mode="widthFix" />
         <view class="relative cu-list menu card-menu sm-border margin-tb-sm shadow-lg">
           <view class="cu-item arrow">
             <button class="cu-btn content" open-type="contact">
@@ -25,9 +25,12 @@
 <script>
 export default {
   data() {
-    return {
-      src: "https://mp-f3138cb7-2a3b-4344-8e79-a1f65871aab2.cdn.bspapp.com/ToolBox365/组织.jpg",
-    };
+    return {};
+  },
+  computed: {
+    user() {
+      return this.$store.state.user || {};
+    },
   },
   onShow() {
     uni.hideShareMenu({
